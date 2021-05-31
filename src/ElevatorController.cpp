eb8224421651b480913cf7103d3e7aa94ba4b99c
Yosry Ahmed <yosrym93@gmail.com>
2021-05-31 21:30:51 +0200

    Added a state machine for the elevator controller

--- src/ElevatorController.cpp
+++ src/ElevatorController.cpp
@@ -3,8 +3,27 @@
 
 void ElevatorController::init() {
     Serial.println("Initializing elevator controller");
+    state = IDLE;
+
+    // TODO: initialize pins
 }
 
 void ElevatorController::run() {
-    Serial.println("Running elevator controller");
+    switch (state) {
+        case IDLE:
+            // TODO
+            break;
+        case GOING_UP:
+            // TODO
+            break;
+        case GOING_DOWN:
+            // TODO
+            break;
+        case DOOR_OPENING:
+            // TODO
+            break;
+        case DOOR_CLOSING:
+            // TODO
+            break;
+    } 
 }
--- src/ElevatorController.h
+++ src/ElevatorController.h
@@ -1,4 +1,14 @@
 class ElevatorController {
+private:
+    enum State {
+        IDLE,
+        GOING_UP,
+        GOING_DOWN,
+        DOOR_OPENING,
+        DOOR_CLOSING
+    };
+
+    State state;
 public:
     void init();
     void run();
