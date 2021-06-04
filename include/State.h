830bf896c7a8b2f407074045f55747322ec3bd78
Mahmoud Adas <mido3ds@gmail.com>
2021-06-04 21:11:11 +0200

    zero struct

--- include/State.h
+++ include/State.h
@@ -10,3 +10,7 @@ struct State {
     // don't move if currentLevel == nextLevel
     int currentLevel, nextLevel;
 };
+
+// initial state is all zeroed out
+// use it to declare state: State<N> s = ZERO_STRUCT;
+#define ZERO_STRUCT {0}
--- test/state/state_test.cpp
+++ test/state/state_test.cpp
@@ -1,13 +1,24 @@
 #include <unity.h>
 #include <nextState.h>
 
-void test_example() {
-    TEST_ASSERT_EQUAL(1, 1); // TODO: remove
+void test_zeroed() {
+    State<5> s = ZERO_STRUCT;
+    TEST_ASSERT_EQUAL(0, s.currentLevel);
+    TEST_ASSERT_EQUAL(0, s.nextLevel);
+    for (auto i : s.floorDownButtons) {
+        TEST_ASSERT_EQUAL(0, i);
+    }
+    for (auto i : s.floorUpButtons) {
+        TEST_ASSERT_EQUAL(0, i);
+    }
+    for (auto i : s.numpad) {
+        TEST_ASSERT_EQUAL(0, i);
+    }
 }
 
 int main() {
     UNITY_BEGIN();
-    RUN_TEST(test_example);
+    RUN_TEST(test_zeroed);
     UNITY_END();
 
     return 0;
