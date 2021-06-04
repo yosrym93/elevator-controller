c3d54735c54b3023df617fd2c3bcc6cabf8bba44
Mahmoud Adas <mido3ds@gmail.com>
2021-06-04 23:59:02 +0200

    numfloors is uint8_t

--- lib/state/src/nextState.h
+++ lib/state/src/nextState.h
@@ -3,7 +3,7 @@
 #ifdef UNIT_TEST
 #include <array>
 
-template<size_t N>
+template<uint8_t N>
 using Bits = std::array<bool, N>;
 #else
 #include <BitBool.h>
@@ -13,7 +13,7 @@ using Bits = std::array<bool, N>;
 
 // assuming buttons are not sticky
 // Bi=1 as long as someone is pressing, otherwise Bi=0
-template<size_t NumFloors>
+template<uint8_t NumFloors>
 struct Input {
     // every floor has one floorUpButton + one floorDownButton + one door sensor
     Bits<NumFloors> floorUpButtons, floorDownButtons, doorsSensors;
@@ -22,20 +22,20 @@ struct Input {
     Bits<NumFloors> numpad;
 };
 
-template<size_t NumFloors>
+template<uint8_t NumFloors>
 struct State {
     // remember the state of the buttons
     Bits<NumFloors> floorUpButtons, floorDownButtons, numpad;
 
     // don't move if currentFloor == nextFloor
-    int currentFloor, nextFloor;
+    uint8_t currentFloor, nextFloor;
 };
 
 // initial state is all zeroed out
 // use it to declare state: State<N> s = ZERO_STRUCT;
 #define ZERO_STRUCT {0}
 
-template<size_t NumFloors>
+template<uint8_t NumFloors>
 inline State<NumFloors> nextState(Input<NumFloors> const* in, State<NumFloors> const* curr) {
     // TODO
     return ZERO_STRUCT;
